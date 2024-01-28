63af3320779c21e679361b885333b89cb7dffb2e
leila <leylag659@gmail.com>
2024-01-28 21:25:07 +0400

    counter with step

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,34 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  let [count, setValue] = useState( 0);
+  const [count, setValue] = useState(0);
+  const [step, setStep] = useState(1);
+
+
   function clickHandler() {
-		setValue(count + 1);
+    if(step > 1){
+		setStep(step - 1);}
 	}
   function clickHandler2() {
-		setValue(count - 1);
+		setStep(step + 1);
 	}
+
+  function clickHandler3(){
+    setValue(count + step)
+  }
+  function clickHandler4(){
+    setValue(count - step)
+  }
 
   return (
     <>
-    <button  onClick={clickHandler2}>-</button>
+    <button onClick={clickHandler}>Step -</button>
+    <div>{step}</div>
+    <button onClick={clickHandler2}>Step +</button>
+    <br/>
+    <button onClick={clickHandler4}>Value -</button>
     <div>{count}</div>
-    <button onClick={clickHandler}>+</button>
+    <button onClick={clickHandler3}>Value +</button>
     </>
   );
 };
